277d9fd7b4c8e1696d9df494f2214e1d6af5d589
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-03-17 14:12:08 +0300

    Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+        let reverseInt = parseInt(n.toString().split('').reverse().join(''));
+        return reverseInt;
+    } else {
+        let reverseInt = parseInt(n.toString().split('').reverse().join(''));
+        return reverseInt;
+    }
 }
